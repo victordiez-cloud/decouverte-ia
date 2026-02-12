93da8bb666a8b86e766ad5adfe4e20773a2c51bf
jonathanbricogne <bricognejonathan@hotmail.fr>
2026-02-12 13:38:07 +0100

    update-design

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@
 const config = {
   // Clé API TMDB - Remplacez par votre propre clé
   // Obtenez votre clé sur: https://www.themoviedb.org/settings/api
-  API_KEY: "VOTRE_CLE_API_ICI",
+  API_KEY: "d601eb629ac424f6f0c3bdd3050037f9",
 
   // URL de base de l'API
   BASE_URL: "https://api.themoviedb.org/3",
